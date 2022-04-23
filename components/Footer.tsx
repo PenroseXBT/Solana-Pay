63142c54c9940072d3f6a75f336878953713ffed
PenroseXBT <algorithmkillers@aol.com>
2022-04-23 11:52:39 -0400

    Devnet
    Devnet USDC Solana Pay /shop

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
+      <a
+        href='https://www.twitter.com/penrosexbt'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Built by{" "}
+        <span>PenroseXBT</span>
+      </a>
+    </footer>
+  )
+}
---
+++ lib/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 'half-pound',
+    name: 'One Half Pound',
+    description: 'A half pound of artisanal roast',
+    unitName: 'pouch', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.05,
+    priceUsd: 8,
+  },
+  {
+    id: 'whole-pound',
+    name: 'Whole Pound',
+    description: 'A whole pound of artisanal roast ',
+    unitName: 'box',
+    priceSol: 0.1,
+    priceUsd: 12,
+  }
+]
